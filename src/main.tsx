4343b33272422cbd24c4ee2bd262dd5692fbf79e
Samir Shaban <samirshabanpsn@gmail.com>
2025-09-28 14:11:59 +0200

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import 'node_modules/modern-normalize/modern-normalize.css';
-import './index.css';
+
 import App from './components/App/App.tsx';
 
 createRoot(document.getElementById('root')!).render(
